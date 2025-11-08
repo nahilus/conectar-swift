78fc365e82d38a3b90b877dc46c265a6aa3ad3a7
Nahilus <ngnahilus@gmail.com>
2025-11-08 12:49:41 +0800

    Initial Commit

---
+++ Conectar/ConectarApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConectarApp.swift
+//  Conectar
+//
+//  Created by Event on 8/11/25.
+//
+
+import SwiftUI
+
+@main
+struct ConectarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
